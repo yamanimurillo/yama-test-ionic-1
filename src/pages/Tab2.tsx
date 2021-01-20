4ca128aa070c906faa61f37753fb6cf6041dbfa5
yamanimurillo <yamani.murillo@gmail.com>
2021-01-20 15:10:57 -0600

    Fix warnings

--- src/pages/Tab2.tsx
+++ src/pages/Tab2.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { IonContent, IonHeader, IonAvatar, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Tab2.css';
 
