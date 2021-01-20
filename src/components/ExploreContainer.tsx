11c339eb48e46c7a06b9cc8897104879c5efd36e
yamanimurillo <yamani.murillo@gmail.com>
2021-01-20 15:03:39 -0600

    Add profile image

--- src/components/ExploreContainer.tsx
+++ src/components/ExploreContainer.tsx
@@ -1,3 +1,5 @@
+
+import { IonAvatar } from '@ionic/react';
 import React from 'react';
 import './ExploreContainer.css';
 
@@ -9,6 +11,13 @@ interface ContainerProps {
 const ExploreContainer: React.FC<ContainerProps> = ({ name, position }) => {
   return (
     <div className="container">
+      
+      <div style={{ textAlign: "center", marginBottom: "12px" }}>
+        <IonAvatar style={{ margin: "0 auto" }}>
+          <img alt='' src="https://ionicframework.com/docs/demos/api/avatar/avatar.svg" />
+        </IonAvatar>
+      </div>
+
       <strong>{name}</strong>
       <p>{position}</p>
     </div>
--- src/pages/Tab2.tsx
+++ src/pages/Tab2.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonContent, IonHeader, IonAvatar, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Tab2.css';
 
@@ -17,8 +17,10 @@ const Tab2: React.FC = () => {
             <IonTitle size="large">Tab 2</IonTitle>
           </IonToolbar>
         </IonHeader>
+        
         <ExploreContainer name="Daniel Antonio Briones Pérez" position='Desarrollador Software Sr' />
       </IonContent>
+
     </IonPage>
   );
 };
